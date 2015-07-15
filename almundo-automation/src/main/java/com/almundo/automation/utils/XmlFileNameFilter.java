ee7f5e4cb01cff3a4e62a249d09b837d49a94be6
Zenen <zenen.morales@globant.com>
2015-07-15 16:20:32 -0500

    Agregar Xtream y clases para leer archivos xml

---
+++ almundo-automation/src/main/java/com/almundo/automation/utils/XmlFileNameFilter.java
@@ -0,0 +1,17 @@
+package com.almundo.automation.utils;
+
+import java.io.*;
+
+/**
+ * This class represents a filename filter to select only the files named like is set the okFileExtensions attribute.
+ * This class implements the Java FilenameFilter interface. 
+ * @author rirala
+ */
+public class XmlFileNameFilter implements FilenameFilter {
+
+	private final String okFileExtensions = "xml";
+
+	public boolean accept(File dir, String name) {
+		return name.endsWith(okFileExtensions);
+	}
+}
