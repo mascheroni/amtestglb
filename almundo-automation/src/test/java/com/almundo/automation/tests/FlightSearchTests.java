3ee7f2f69cf7ea243e279f80286ec4b3d4721396
Zenen <zenen.morales@globant.com>
2015-07-17 14:40:22 -0500

    Add test verifyAirportsOnFiltersResponse

--- almundo-automation/src/test/java/com/almundo/automation/tests/FlightSearchTests.java
+++ almundo-automation/src/test/java/com/almundo/automation/tests/FlightSearchTests.java
@@ -3,59 +3,52 @@ package com.almundo.automation.tests;
 import java.util.List;
 import java.util.Map;
 
-import junit.framework.AssertionFailedError;
-
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.almundo.automation.entities.Airline;
 import com.almundo.automation.entities.Cluster;
+import com.almundo.automation.entities.Filter;
+import com.almundo.automation.entities.Value;
 import com.almundo.automation.services.Response;
 import com.almundo.automation.utils.DataProviders;
 
 public class FlightSearchTests extends BaseTest {
 
-	
-	@Test(description = "Test that verifies if the airlines names are not null",
-		  groups = { "flight-search" },
-		  dataProvider = "test1", dataProviderClass = DataProviders.class)
+	@Test(description = "Test that verifies if the airlines names are not null", groups = { "flight-search" }, dataProvider = "test1", dataProviderClass = DataProviders.class)
 	public void verifyAirlineNamesNotNull(Map<String, String> data) {
-		String reqDate = 
-				this.utils.convertToSpecifDate(data.get("date"));
+		String reqDate = this.utils.convertToSpecifDate(data.get("date"));
 		data.remove("date");
 		data.put("departure", reqDate);
-		
+
 		this.httpClient.setSearchRequest(data);
 		Response response = this.httpClient.post();
-		
+
 		List<Airline> airlines = response.getSearchFlights()
 				.getLowestPricesByAirline().getAirlines();
-		
-		for(Airline airline: airlines) {
-			if(airline.getName().toUpperCase().equals("NULL")){
-				Assert.fail("The airline " + airline.getCode()+ " with "
+
+		for (Airline airline : airlines) {
+			if (airline.getName().toUpperCase().equals("NULL")) {
+				Assert.fail("The airline " + airline.getCode() + " with "
 						+ "catalog ID " + airline.getCatalog_id() + " has "
 						+ "a null value instead of its name");
 			}
 		}
 	}
-	
-	
+
 	@Test(description = "Test that verifies that given a number of passengers, "
-			+ "the total amount be equals to the detailed amount per passenger",
-		  groups = { "flight-search" },
-		  dataProvider = "test1", dataProviderClass = DataProviders.class)
+			+ "the total amount be equals to the detailed amount per passenger", groups = { "flight-search" }, dataProvider = "test1", dataProviderClass = DataProviders.class)
 	public void verifyPricesAcordingToNumberOfPax(Map<String, String> data) {
-		String reqDate = 
-				this.utils.convertToSpecifDate(data.get("date"));
+		String reqDate = this.utils.convertToSpecifDate(data.get("date"));
 		data.remove("date");
 		data.put("departure", reqDate);
-		
+
 		this.httpClient.setSearchRequest(data);
 		Response response = this.httpClient.post();
-		
-		List<Cluster> clusters = response.getSearchFlightsAndClusters().getClusters();
-		
+
+		List<Cluster> clusters = response.getSearchFlightsAndClusters()
+				.getClusters();
+
 		for (Cluster cluster : clusters) {
 			float adultPrice = cluster.getPrice().getDetail().getAdultPrice();
 			float childPrice = cluster.getPrice().getDetail().getChildPrice();
@@ -63,37 +56,60 @@ public class FlightSearchTests extends BaseTest {
 			float taxPrice = cluster.getPrice().getDetail().getTaxes();
 			float extraTax = cluster.getPrice().getDetail().getExtra_tax();
 			float total = cluster.getPrice().getTotal();
-			float sum = (adultPrice * Float.parseFloat(data.get("adults"))) + 
-						(childPrice * Float.parseFloat(data.get("children"))) +
-						(infantPrice * Float.parseFloat(data.get("infants"))) +
-						taxPrice + extraTax;
+			float sum = (adultPrice * Float.parseFloat(data.get("adults")))
+					+ (childPrice * Float.parseFloat(data.get("children")))
+					+ (infantPrice * Float.parseFloat(data.get("infants")))
+					+ taxPrice + extraTax;
 			if (total != sum) {
-				Assert.fail("The carrier "+ cluster.getValidating_carrier()
+				Assert.fail("The carrier " + cluster.getValidating_carrier()
 						+ " should have an amount of " + total
 						+ ", but it has " + sum);
 			}
 		}
 	}
-	
+
 	@Test(description = "Verifies that given an international itinerary, "
-			+ "the domestic field be false",
-		  groups = { "flight-search" },
-		  dataProvider = "test1", dataProviderClass = DataProviders.class)
+			+ "the domestic field be false", groups = { "flight-search" }, dataProvider = "test1", dataProviderClass = DataProviders.class)
 	public void verifyDomesticFieldFalse(Map<String, String> data) {
-		String reqDate = 
-				this.utils.convertToSpecifDate(data.get("date"));
+		String reqDate = this.utils.convertToSpecifDate(data.get("date"));
 		data.remove("date");
 		data.put("departure", reqDate);
-		
+
 		this.httpClient.setSearchRequest(data);
 		Response response = this.httpClient.post();
-		
-		List<Cluster> clusters = response.getSearchFlightsAndClusters().getClusters();
-		
-		for (Cluster cluster: clusters) {
-			Assert.assertFalse(cluster.isDomestic(),
-					"The International itinerary operated by " + cluster.getValidating_carrier() 
-					+ " has the domestic field TRUE");
+
+		List<Cluster> clusters = response.getSearchFlightsAndClusters()
+				.getClusters();
+
+		for (Cluster cluster : clusters) {
+			Assert.assertFalse(
+					cluster.isDomestic(),
+					"The International itinerary operated by "
+							+ cluster.getValidating_carrier()
+							+ " has the domestic field TRUE");
+		}
+	}
+
+	@Test(description = "Verify that the aiports on the filters section are not null", groups = { "flight-search" }, dataProvider = "test1", dataProviderClass = DataProviders.class)
+	public void verifyAirportsOnFiltersResponse(Map<String, String> data) {
+		String reqDate = this.utils.convertToSpecifDate(data.get("date"));
+		data.remove("date");
+		data.put("departure", reqDate);
+
+		this.httpClient.setSearchRequest(data);
+		Response response = this.httpClient.post();
+
+		List<Filter> filters = response.getFilters();
+		List<Value> values;
+
+		for (Filter filter : filters) {
+			values = filter.getValues();
+			for (Value value : values) {
+				if (value.getCode().equals("null"))
+					Assert.fail("The code for " + value.getName() + " is null");
+				if (value.getName().equals("null"))
+					Assert.fail("The name for " + value.getCode() + " is null");
+			}
 		}
 	}
 
