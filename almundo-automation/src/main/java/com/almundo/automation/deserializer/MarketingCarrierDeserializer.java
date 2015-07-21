b276d265c94f2ebee9c05a6ffcd73f0bb63e4b06
Estefanie Canseco <estefanie.canseco@globant.com>
2015-07-21 10:53:30 -0500

    Se sube el deserializador de marketing carrier

---
+++ almundo-automation/src/main/java/com/almundo/automation/deserializer/MarketingCarrierDeserializer.java
@@ -0,0 +1,34 @@
+package com.almundo.automation.deserializer;
+
+import java.lang.reflect.Type;
+
+import com.almundo.automation.entities.MarketingCarrier;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonPrimitive;
+
+public class MarketingCarrierDeserializer implements JsonDeserializer<MarketingCarrier> {
+
+	@Override
+	public MarketingCarrier deserialize(JsonElement json, Type typeOfT,
+			JsonDeserializationContext context) throws JsonParseException {
+		final JsonObject jsonObject = json.getAsJsonObject();
+		final JsonObject jsonCarrierObject = jsonObject.getAsJsonObject("marketing_carrier");
+
+		MarketingCarrier carrier;
+
+		final JsonPrimitive catalogId = (JsonPrimitive) jsonCarrierObject
+				.getAsJsonPrimitive("catalog_id");
+		final JsonObject code = (JsonObject) jsonCarrierObject.get("code");
+		final JsonObject name = (JsonObject) jsonCarrierObject.get("name");
+		carrier = new MarketingCarrier(catalogId.getAsInt(),
+				code.getAsString(), name.getAsString());
+
+		return carrier;
+
+	}
+}
+
